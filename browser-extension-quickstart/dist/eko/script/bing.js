59447121b7726e76d9fc47b9687ae0761621c05e
veasion <lzw@veasion.cn>
2025-02-05 23:27:40 +0800

    chore: modify quickstart to v1.0.9

--- browser-extension-quickstart/dist/eko/script/bing.js
+++ browser-extension-quickstart/dist/eko/script/bing.js
@@ -1,1 +1,25 @@
-eko.getDetailLinks=async function(e){await eko.waitForElementPresent(document.body,"main h2 a",5e3);let t=document.querySelectorAll("main h2 a");0===t.length&&(t=document.querySelectorAll("h2 a"));let n=[];for(let e=0;e<t.length;e++){let l=eko.cleanText(t[e]),o=t[e].getAttribute("href");n.push({title:l,url:o})}return{links:n}},eko.getContent=async function(e){return await eko.waitLoaded(),{title:document.title,content:eko.extractHtmlContent()}};
+
+eko.getDetailLinks = async function (search) {
+    await eko.waitForElementPresent(document.body, 'main h2 a', 5000)
+    let search_list = document.querySelectorAll('main h2 a')
+    if (search_list.length === 0) {
+        search_list = document.querySelectorAll('h2 a')
+    }
+    let links = []
+    for (let i = 0; i < search_list.length; i++) {
+        let title = eko.cleanText(search_list[i])
+        let url = search_list[i].getAttribute('href')
+        links.push({ title, url })
+    }
+    return {
+        links: links
+    }
+}
+
+eko.getContent = async function (search) {
+    await eko.waitLoaded()
+    return {
+        title: document.title,
+        content: eko.extractHtmlContent()
+    }
+}
